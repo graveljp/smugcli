41ccc8df3bbec3f77c70b8a5d3defa3f71346dc4
Jean-Philippe Gravel <jpgravel@gmail.com>
2018-05-27 17:36:37 -0400

    Move test_mkdir_folder_depth_limits closer to the other mkdir tests

--- end_to_end_test.py
+++ end_to_end_test.py
@@ -391,6 +391,20 @@ class EndToEndTest(unittest.TestCase):
     self._do('ls -l {root}/private',
              [ExpectRegex(r'{\n  .*  "Privacy": "Private"')])
 
+  def test_mkdir_folder_depth_limits(self):
+    # Can't create more than 5 folder deep.
+    self._do('mkdir -p {root}/1/2/3/4',
+             ['Creating Folder "{root}".\n'
+              'Creating Folder "{root}/1".\n'
+              'Creating Folder "{root}/1/2".\n'
+              'Creating Folder "{root}/1/2/3".\n'
+              'Creating Folder "{root}/1/2/3/4".\n'])
+    self._do('mkdir -p {root}/1/2/3/4/5',
+             ['Cannot create "{root}/1/2/3/4/5", SmugMug does not support '
+              'folder more than 5 level deep.'])
+    self._do('mkalbum -p {root}/1/2/3/4/5',
+             ['Creating Album "{root}/1/2/3/4/5".'])
+
   def test_mkalbum(self):
     # Missing parent.
     self._do('mkalbum {root}/folder/album',
@@ -587,20 +601,6 @@ class EndToEndTest(unittest.TestCase):
     self._do('sync {root} --privacy=private')
     self._do('ls -l {root}/private',
              [ExpectRegex(r'{\n  .*  "Privacy": "Private"')])
-
-  def test_mkdir_folder_depth_limits(self):
-    # Can't create more than 5 folder deep.
-    self._do('mkdir -p {root}/1/2/3/4',
-             ['Creating Folder "{root}".\n'
-              'Creating Folder "{root}/1".\n'
-              'Creating Folder "{root}/1/2".\n'
-              'Creating Folder "{root}/1/2/3".\n'
-              'Creating Folder "{root}/1/2/3/4".\n'])
-    self._do('mkdir -p {root}/1/2/3/4/5',
-             ['Cannot create "{root}/1/2/3/4/5", SmugMug does not support '
-              'folder more than 5 level deep.'])
-    self._do('mkalbum -p {root}/1/2/3/4/5',
-             ['Creating Album "{root}/1/2/3/4/5".'])
 
   def test_sync_folder_depth_limits(self):
     self._stage_files('{root}/1/2/3/4/album', ['testdata/SmugCLI_1.jpg'])

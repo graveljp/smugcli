8efbcfc6955e05cdf4e5692d1ffbfceb21081f7e
Jean-Philippe Gravel <jpgravel@gmail.com>
2019-03-24 01:38:22 -0400

    Add more test cases in task_manager_test.py

--- task_manager_test.py
+++ task_manager_test.py
@@ -1,9 +1,66 @@
 import task_manager
 
+import datetime
+import freezegun
+import io_expectation as expect
+import itertools
 from parameterized import parameterized
+import sys
 import unittest
 
 class TaskManagerTests(unittest.TestCase):
+
+  def except_status(self, status_string):
+    expect_escape = expect.Regex('\\033\[J|'
+                                 '\\033\[\d+A|'
+                                 '\n|'
+                                 '\r').repeatedly()
+    return expect.InOrder([expect_escape] +
+                          list(itertools.chain.from_iterable(
+                            [s, expect_escape] for s in status_string)))
+
+  def test_update_progress(self):
+    io = expect.ExpectedInputOutput()
+    sys.stdout = io
+    with freezegun.freeze_time('2019-01-01 12:00:00') as frozen_time:
+      with task_manager.TaskManager() as manager:
+        with manager.start_task(0, 'Some task'):
+          frozen_time.tick(delta=datetime.timedelta(milliseconds=1))
+          manager.update_progress(0, 'Some task', ': 25%')
+          frozen_time.tick(delta=datetime.timedelta(milliseconds=100))
+          manager.update_progress(0, 'Some task', ': 50%')
+          frozen_time.tick(delta=datetime.timedelta(milliseconds=1))
+          manager.update_progress(0, 'Some task', ': 75%')
+
+    io.assert_output_was(self.except_status(['Some task',
+                                             'Some task: 50%']))
+
+  def test_task_groups(self):
+    io = expect.ExpectedInputOutput()
+    sys.stdout = io
+    with freezegun.freeze_time('2019-01-01 12:00:00') as frozen_time:
+      with task_manager.TaskManager() as manager:
+        with manager.start_task(0, 'Task 0'):
+          io.assert_output_was(self.except_status(['Task 0']))
+
+          frozen_time.tick(delta=datetime.timedelta(milliseconds=100))
+          with manager.start_task(1, 'Task 1'):
+            io.assert_output_was(self.except_status(['Task 0',
+                                                     'Task 1']))
+
+            frozen_time.tick(delta=datetime.timedelta(milliseconds=100))
+            manager.update_progress(0, 'Task 0', ': 50%')
+            io.assert_output_was(self.except_status(['Task 0: 50%',
+                                                     'Task 1']))
+
+            frozen_time.tick(delta=datetime.timedelta(milliseconds=100))
+            manager.update_progress(1, 'Task 1', ': 75%')
+            io.assert_output_was(self.except_status(['Task 0: 50%',
+                                                     'Task 1: 75%']))
+
+          io.assert_output_was(self.except_status(['Task 0: 50%']))
+        io.assert_output_was(self.except_status([]))
+
   @parameterized.expand([
       ('1234567890123456789', '1234567890123456789'),
       ('12345678901234567890', '12345678901234567890'),

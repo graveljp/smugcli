377d368eb0bb30a6099ebd68fecf01a05d7a97b7
Jean-Philippe Gravel <jpgravel@gmail.com>
2020-12-13 16:42:09 -0500

    Bump version number to 1.0.3

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="smugcli",
-  version="1.0.2",
+  version="1.0.3",
   author="Jean-Philippe Gravel",
   author_email="jpgravel@gmail.com",
   description="Command line tool for SmugMug",
